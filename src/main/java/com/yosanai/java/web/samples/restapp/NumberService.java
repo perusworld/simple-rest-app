cb3e4a644b6727fb7c9cfda34b4050038c612f26
Saravana P Shanmugam <me@perusworld.com>
2010-08-21 09:51:37 -0700

    initial commit

---
+++ src/main/java/com/yosanai/java/web/samples/restapp/NumberService.java
@@ -0,0 +1,60 @@
+/**
+ * com.yosanai.java.web.samples.restapp.NumberService
+ *
+ * This is the MIT License
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+package com.yosanai.java.web.samples.restapp;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Saravana P Shanmugam
+ * 
+ */
+@Path("/number/")
+@Component
+@Scope("singleton")
+public class NumberService {
+
+    @POST
+    @Consumes( { "application/x-www-form-urlencoded" })
+    @Produces( { "text/plain" })
+    @Path("add")
+    public Response add(@FormParam("numberA") int numberA, @FormParam("numberB") int numberB) {
+        Response ret = null;
+        ResponseBuilder responseBuilder = Response.ok();
+        responseBuilder.entity("" + (numberA + numberB));
+        ret = responseBuilder.build();
+        return ret;
+    }
+
+}
